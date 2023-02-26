7cb866f174b6d5fe1219d5007d9b1472c80602a2
JCPenne <jackcpalmer@icloud.com>
2023-02-26 13:39:31 -0800

    implement basic landing page design and first route to page

---
+++ components/Button/Button.js
@@ -0,0 +1,12 @@
+import styles from './Button.module.css';
+
+export default function Button({ children }) {
+  return (
+    <button
+      type='button'
+      className={styles.filled}
+    >
+      {children}
+    </button>
+  );
+}
