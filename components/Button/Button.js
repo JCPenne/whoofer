be188f9f4564000031ce5339ec6fab785df5ef12
JCPenne <jackcpalmer@icloud.com>
2023-03-21 16:00:41 -0700

    add bounce animation to buttons

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -1,14 +1,20 @@
 import styles from './Button.module.css';
 
+import { Pacifico } from 'next/font/google';
+const pacifico = Pacifico({
+  weight: '400',
+  subsets: ['latin'],
+});
+
 export default function Button({ children, disabled, onClick }) {
   return (
-    <button
-      type='button'
-      className={styles.filled}
-      disabled={disabled}
-      onClick={onClick && (() => onClick(true))}
-    >
-      {children}
-    </button>
+      <button
+        type='button'
+        className={styles.filled}
+        disabled={disabled}
+        onClick={onClick && (() => onClick(true))}
+      >
+        {children}
+      </button>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
 import Head from 'next/head';
 import Link from 'next/link';
-import { Inter } from 'next/font/google';
+
+
+
 import styles from '@/styles/Home.module.css';
 
 import Button from '@/components/Button/Button';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
@@ -21,27 +21,34 @@ export default function Home() {
           content='width=device-width, initial-scale=1'
         />
       </Head>
-      <main className={styles.main}>
-        <h1 className={styles.title}>Whoofer</h1>
-        <p className={styles.description}>
-          Welcome to Whoofer!
-          <br />
-          The Dog Quiz made by dogs for humans.
-          <br />
-          Pick a game to get started!
-        </p>
-        <div className={styles.buttonArray}>
-          <Link href='/quiz'>
-            <Button>Quiz</Button>
-          </Link>
-          <Link href='/Fetch'>
-            <Button>Fetch</Button>
-          </Link>
-          <Link href='/HideAndSeek'>
-            <Button>Hide & Seek</Button>
-          </Link>
-        </div>
-      </main>
+      <h1 className={styles.title}>Whoofer</h1>
+      <p className={styles.description}>
+        Welcome to Whoofer!
+        <br />
+        The Dog Quiz made by dogs for humans.
+        <br />
+        Pick a game to get started!
+      </p>
+      <div className={styles.buttonArray}>
+        <Link
+          href='/quiz'
+          className={styles.link}
+        >
+          <Button>Quiz</Button>
+        </Link>
+        <Link
+          href='/Fetch'
+          className={styles.link}
+        >
+          <Button>Fetch</Button>
+        </Link>
+        <Link
+          href='/HideAndSeek'
+          className={styles.link}
+        >
+          <Button>Hide & Seek</Button>
+        </Link>
+      </div>
     </>
   );
 }
